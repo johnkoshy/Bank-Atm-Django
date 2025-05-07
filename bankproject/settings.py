b1a14b2f514b9994cef3794184e7579adb6611b8
John <john.koshy@outlook.com>
2025-05-07 12:54:03 +0530

    Updated static files configuration in settings.py

--- bankproject/settings.py
+++ bankproject/settings.py
@@ -92,6 +92,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
